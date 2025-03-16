c07f2fb85d30e5683230c0d494e483247b2e670d
Srinath - ASUS <srinathdivate123@gmail.com>
2025-03-16 15:24:24 +0530

    favicon

--- src/components/logo/index.tsx
+++ src/components/logo/index.tsx
@@ -5,6 +5,7 @@ const Logo = (props: { url?: string }) => {
   const { url = "/" } = props;
   return (
     <div className="flex items-center justify-center sm:justify-start">
+      
       <Link to={url}>
         <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
           <AudioWaveform className="size-4" />
